6da0717d05ea6de3082a2b96ef31c62837a67121
M Zidane <92719420+moooozi@users.noreply.github.com>
2025-11-24 16:33:28 +0100

    Moved local crate firmware_variables library to an external repository

--- src-tauri/libs/firmware_variables/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod boot;
-pub mod device_path;
-pub mod load_option;
-pub mod privileges;
-pub mod utils;
-pub mod variables;
--- src-tauri/libs/firmware_variables/src/privileges/mod.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(windows)]
-pub mod windows;
-#[cfg(windows)]
-pub use windows::*;
-
-#[cfg(unix)]
-pub mod unix;
-#[cfg(unix)]
-pub use unix::*;
